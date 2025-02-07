31ca6a03993cf5142db136acfb7e0cf9eb0a2f96
nitin2991 <nitin.kamboj510@gmail.com>
2025-02-07 21:14:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 API_ID = "29801489"  # Replace this API ID with your actual API ID
 API_HASH = "74aaff69f6cacf1239d6a6f38a24374c"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "BQHGvBEAIvp4nNokVpgwgPc2PDMFdj--dxvE8dsKSK_MBVlteWjYk5_zNf-sdqev8dLPBPuRvbJZwWSjUd1Nw1-gKZI--mFYfZka6jGatdp-JefrP6AnviFaBL1ZfRiVNYnwJRU3oabMi-ZUCax_S4pcT8QOAZw0OAY4pSMOmkkCvrwO-6o5b2Ic-Ii0RadrjyoUPsSOKXeG40B6n3TRC_wTixPJH-jwo0BJrNsRlZV5FI8lEsM50u1fc9ZGlZOfegsGt1AZrX0ZlnMzHKZwi6KChSScU4_XBsKfLXZdX_nqYci-7WAedzbum793QWzAzXwIvfrKQ3rPzFJRu1Kkhr5Mk9gKQwAAAAGNZpJOAA"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+BOT_TOKEN = "8149870803:AAEGkM-GSTRCyZl5_-udrqK2CPbNv6odu-A"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [12345678, 12345678]
+ADMIN_IDS = [6667276878]
 
 # Limits
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
